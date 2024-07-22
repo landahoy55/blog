ecdf398c41faa1863883b05b91db91603c2ec519
Pete <landahoy55@gmail.com>
2024-07-22 17:43:28 +0100

    Introduced post schema and model
    Co-authored-by: clifflord <clifflord@users.noreply.github.com>

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 //routes
 router.get('', (req, res) => {
     const locals = {
-        title: "NodeJs Blog",
+        title: "This is a test title",
         description: "Simple blog created in NodeJs, Express & MongoDb."
     }
-
 
     res.render('index', { locals });
 });
@@ -16,4 +16,27 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+/** 
+GET
+HOME
+**/
+
+function insertPostData() {
+    Post.insertMany([
+        {
+           title: "First Post!",
+           body: "The body!"  
+        },
+        {
+            title: "Second Post!",
+            body: "More lovely bodies!" 
+        },  
+        {
+            title: "Third Post!",
+            body: "Bodies!!" 
+        }  
+    ])
+}
+
+insertPostData();
 module.exports = router;
