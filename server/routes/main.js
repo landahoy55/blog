6a2033a287bdf5dd8b5b6a334c227eb582d76816
Matthew Clifford <matth3wclifford@gmail.com>
2024-07-22 19:08:15 +0100

    22 July Co-authored-by: Pete Malone <landahoy55@users.noreply.github.com>

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,13 +3,45 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //routes
-router.get('', (req, res) => {
-    const locals = {
-        title: "This is a test title",
-        description: "Simple blog created in NodeJs, Express & MongoDb."
+router.get('', async (req, res) => {
+
+    try {
+        const locals = {
+            title: "This is a test title",
+            description: "Simple blog created in NodeJs, Express & MongoDb."
+        }
+
+        //Pagination logic
+        //Define number of articles per page
+        let perPage = 10;
+
+        //Get current page
+        let page = req.query.page || 1;
+
+        //A chained query to retrieve the data.
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        //Count all the posts
+        const count = await Post.countDocuments();
+        //Get the next page number
+        const nextPage = parseInt(page) + 1;
+        //Work out if there is a next page
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+    } catch (error) {
+        console.log(error);
     }
 
-    res.render('index', { locals });
+
 });
 
 router.get('/about', (req, res) => {
@@ -21,22 +53,25 @@ GET
 HOME
 **/
 
-function insertPostData() {
-    Post.insertMany([
-        {
-           title: "First Post!",
-           body: "The body!"  
-        },
-        {
-            title: "Second Post!",
-            body: "More lovely bodies!" 
-        },  
-        {
-            title: "Third Post!",
-            body: "Bodies!!" 
-        }  
-    ])
-}
 
-insertPostData();
 module.exports = router;
+
+
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//            title: "First Post!",
+//            body: "The body!"  
+//         },
+//         {
+//             title: "Second Post!",
+//             body: "More lovely bodies!" 
+//         },  
+//         {
+//             title: "Third Post!",
+//             body: "Bodies!!" 
+//         }  
+//     ])
+// }
+
+// insertPostData();
